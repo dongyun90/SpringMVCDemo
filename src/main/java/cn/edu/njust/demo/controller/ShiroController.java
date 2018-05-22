6832d0b712553d3335194190a4732558f521a397
dongyun <dongyun_1990@163.com>
2018-05-22 20:07:52 +0800

    Set @RestController for SpringMVC

---
+++ src/main/java/cn/edu/njust/demo/controller/ShiroController.java
@@ -0,0 +1,30 @@
+package cn.edu.njust.demo.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/shiro")
+public class ShiroController {
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public Map<Object, Object> test(@RequestParam Map<Object, Object> params) {
+        return params;
+    }
+}
